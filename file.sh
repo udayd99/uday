248bb335a29d226ce82d2c7ff665ab9f5fe5e991
uday <udayagm966@gmail.com>
2022-06-01 13:03:16 +0000

    new file which one is big

---
+++ file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##this script is used for find the big number
+echo "enter a number "
+read a
+echo " enter b value :"
+read b
+if [ $a -gt $b  ]
+then 
+echo " $a is a is big"
+else echo " $b b is big"
+fi
+
